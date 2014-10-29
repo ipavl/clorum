306d412231031d18b5756f2e6826b0737e82280f
ipavl <ipavl@users.sourceforge.net>
2014-10-28 22:12:35 -0400

    Updating jdbc dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ipavl/clorum"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.clojure/java.jdbc "0.3.6"]
                  [compojure "1.2.0"]
                  [java-jdbc/dsl "0.1.0"]
                  [ring/ring-defaults "0.1.2"]
