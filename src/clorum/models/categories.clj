5e253cff36ab53db70cdd439fa0ecf29c4433698
ipavl <ipavl@users.sourceforge.net>
2014-11-09 10:47:31 -0500

    Referencing the db function directly instead of through a proxy function

--- src/clorum/models/categories.clj
+++ src/clorum/models/categories.clj
@@ -4,16 +4,13 @@
             [java-jdbc.sql :as sql]
             [clorum.core.config :as config]))
 
-(def db
-  config/db)
-
 (def timeNow
   (str (java.sql.Timestamp.(System/currentTimeMillis))))
 
 (defn all []
-  (jdbc/query db
+  (jdbc/query config/db
               ["SELECT category, COUNT(1) AS discussions FROM discussions GROUP BY category ORDER BY discussions DESC"]))
 
 (defn get [category]
-  (jdbc/query db
+  (jdbc/query config/db
               (sql/select * :discussions (sql/where {:category category}))))
--- src/clorum/models/discussions.clj
+++ src/clorum/models/discussions.clj
@@ -4,33 +4,30 @@
             [java-jdbc.sql :as sql]
             [clorum.core.config :as config]))
 
-(def db
-  config/db)
-
 (def timeNow
   (str (java.sql.Timestamp.(System/currentTimeMillis))))
 
 (defn all []
-  (jdbc/query db
+  (jdbc/query config/db
               (sql/select * :discussions)))
 
 (defn get [id]
-  (first (jdbc/query db
+  (first (jdbc/query config/db
                      (sql/select * :discussions (sql/where {:id id})))))
 
 (defn get-replies [parent]
-  (jdbc/query db
+  (jdbc/query config/db
               (sql/select * :replies (sql/where {:parent parent}))))
 
 (defn create [params]
-  (jdbc/insert! db :discussions (merge params {:created timeNow :modified timeNow})))
+  (jdbc/insert! config/db :discussions (merge params {:created timeNow :modified timeNow})))
 
 (defn create-reply [params]
-  (jdbc/insert! db :replies (merge params {:created timeNow :modified timeNow})))
+  (jdbc/insert! config/db :replies (merge params {:created timeNow :modified timeNow})))
 
 (defn save [id params]
-  (jdbc/update! db :discussions params (sql/where {:id id})))
+  (jdbc/update! config/db :discussions params (sql/where {:id id})))
 
 (defn delete [id]
-  (jdbc/delete! db :discussions (sql/where {:id id}))
-  (jdbc/delete! db :replies (sql/where {:parent id})))
+  (jdbc/delete! config/db :discussions (sql/where {:id id}))
+  (jdbc/delete! config/db :replies (sql/where {:parent id})))
