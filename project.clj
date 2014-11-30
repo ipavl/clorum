a6d0e264b2937b4e94cd4b610256cffb23e8282e
ipavl <ipavl@users.sourceforge.net>
2014-11-29 23:40:59 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.jdbc "0.3.6"]
-                 [compojure "1.2.0"]
-                 [java-jdbc/dsl "0.1.0"]
+                 [compojure "1.2.1"]
+                 [java-jdbc/dsl "0.1.1"]
                  [ring/ring-defaults "0.1.2"]
                  [mysql/mysql-connector-java "5.1.33"]
                  [de.ubercode.clostache/clostache "1.4.0"]
