f43f278b03bb6cab5b5d2f5ac97c2a0d1d1045e2
ipavl <ipavl@users.sourceforge.net>
2014-12-04 21:07:15 -0500

    Fix users model documentation

--- src/clorum/models/users.clj
+++ src/clorum/models/users.clj
@@ -5,31 +5,37 @@
             [clorum.core.config :as config]
             [clorum.core.util :as util]))
 
-(defn all []
+(defn all
   "Returns all rows in the users table."
+  []
   (jdbc/query config/db
               (sql/select * :users)))
 
-(defn get [id]
+(defn get
   "Returns the user with the specified id."
+  [id]
   (first (jdbc/query config/db
                      (sql/select * :users (sql/where {:id id})))))
 
-(defn get-by-name [username]
+(defn get-by-name
   "Returns the user with the specified name."
+  [username]
   (first (jdbc/query config/db
                      (sql/select * :users (sql/where {:username (first username)})))))
 
-(defn create [params]
+(defn create
   "Inserts a new user with the passed parameters."
+  [params]
   (if (nil? (get-by-name [(:username params)]))
     (jdbc/insert! config/db :users (merge params {:registered util/current-time-sql
                                                 :password (util/encrypt (:password params))}))))
 
-(defn save [id params]
+(defn save
   "Updates the user with the specified id with the passed parameters."
+  [id params]
   (jdbc/update! config/db :users params (sql/where {:id id})))
 
-(defn delete [id]
+(defn delete
   "Deletes the user with the specified id."
+  [id]
   (jdbc/delete! config/db :users (sql/where {:id id})))
