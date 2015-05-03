d6c187a3420afc37c8ab98b3d83eed95cd18f52f
ipavl <ipavl@users.sourceforge.net>
2015-05-03 14:20:20 -0400

    Colour post background differently if verified

--- resources/public/assets/js/clorum.js
+++ resources/public/assets/js/clorum.js
@@ -1,8 +1,13 @@
-// Prettify post verification indicators
-$('.post-verified').each(function() {
-  if ($(this).text() === "(verified: 1)") {
-    $(this).html("Verified");
+// Loop through each post element
+$('.post').each(function() {
+  var verifiedLabel = $(this).find('p.post-verified');
+
+  // Prettify post verification indicators
+  if (verifiedLabel.text() === "(verified: 1)") {
+    $(this).addClass("is-verified");
+
+    verifiedLabel.html("Verified");
   } else {
-    $(this).html("");
+    verifiedLabel.html("");
   }
 });
