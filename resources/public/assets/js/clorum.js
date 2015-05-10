8e9555749cabf5b7b4784777ca0e0384adb6d5a6
ipavl <ipavl@users.sourceforge.net>
2015-05-10 15:50:23 -0400

    Update checked string in verify JS function

--- resources/public/assets/js/clorum.js
+++ resources/public/assets/js/clorum.js
@@ -3,7 +3,7 @@ $('.post').each(function() {
   var verifiedLabel = $(this).find('p.post-verified');
 
   // Prettify post verification indicators
-  if (verifiedLabel.text() === "(verified: 1)") {
+  if (verifiedLabel.text() === "(verified: true)") {
     $(this).find("p.post-author").addClass("is-verified");
   } else {
     $(this).find("p.post-author").addClass("is-guest");
